59085c594231b7c79c33c646d141a026be805de4
Jeremy Kithome <jeremy.kithome@andela.com>
2017-01-05 11:45:59 +0300

    Voting_app: Add first component

---
+++ voting_app/app.js
@@ -0,0 +1,13 @@
+const ProductList = React.createClass({
+  render: function () {
+    return (
+      <div className='ui items'>
+        Hello, friend! I am a basic React component
+      </div>
+    );
+  },
+});
+
+
+ReactDOM.render(<ProductList />, document.getElementById('content')
+);
