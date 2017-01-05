fe973aaa8d4bfbf0fbbab76043daba895515f069
Jeremy Kithome <jeremy.kithome@andela.com>
2017-01-05 22:14:04 +0300

    Voting_app: Apply final styling

--- voting_app/app.js
+++ voting_app/app.js
@@ -87,21 +87,38 @@ const Product = React.createClass({
           <img src={this.props.product_image_url} />
         </div>
         <div className='middle aligned content'>
-          <div className='header'>
-            <a onClick={this.handleUpVote}>
-              <i className='large caret up icon'></i>
-            </a>
-            <a onClick={this.handleDownVote}>
-              <i className='large caret down icon'></i>
-            </a>
-            {this.props.votes}
-          </div>
-          <div className='description'>
-            <a href={this.props.url}>{this.props.title}</a>
-          </div>
-          <div className='extra'>
-            <span>Submitted by: </span>
-            <img className='ui avatar image' src={this.props.submitter_avatar_url} />
+          <div className='ui grid'>
+            <div className='three wide column'>
+              <div className='ui basic center aligned segment'>
+                <a onClick={this.handleUpVote}>
+                  <i className='large caret up icon'></i>
+                </a>
+                <a onClick={this.handleDownVote}>
+                  <i className='large caret down icon'></i>
+                </a>
+                <p><b>{this.props.votes}</b></p>
+              </div>
+            </div>
+            <div className='twelve wide column'>
+              <div className='header'>
+                <a href={this.props.url}>
+                  {this.props.title}
+                </a>
+              </div>
+              <div className='meta'>
+                <span></span>
+              </div>
+              <div className='description'>
+                <p>{this.props.description}</p>
+              </div>
+              <div className='extra'>
+                <span>Submitted by:</span>
+                <img
+                  className='ui avatar image'
+                  src={this.props.submitter_avatar_url}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
